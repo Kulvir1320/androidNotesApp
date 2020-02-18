55b1a217cd5ec4c0c2923972d2ef000434f0dce5
evneet <nancymaan4@gmail.com>
2020-02-17 19:40:34 -0500

    floating action button added

--- app/src/main/java/com/example/myapplication/NotesActivity.java
+++ app/src/main/java/com/example/myapplication/NotesActivity.java
@@ -15,19 +15,11 @@ import android.widget.GridView;
 public class NotesActivity extends AppCompatActivity {
     GridView gridView;
     int[] imageIcons = {R.drawable.icon01_01};
-    //    R.drawable.icon01_02, R.drawable.icon01_03, R.drawable.icon01_04,
-//            R.drawable.icon01_05, R.drawable.icon01_06, R.drawable.icon01_07, R.drawable.icon01_08, R.drawable.icon01_09,
-//            R.drawable.icon01_10};
+
     String[] title = {"one"};
-    //            "one", "two", "three",
-//            "four", "five", "six",
-//            "seven", "eight", "nine", "ten"
-//    };
+
     String[] desc = {"one"};
-//            "one", "two", "three",
-//            "four", "five", "six",
-//            "seven", "eight", "nine", "ten"
-//    };
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
